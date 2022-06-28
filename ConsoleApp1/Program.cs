b2e5ca1484d42b5ffe04a4aa18bb912c08e7ae0e
Андрей <nevraev1109@mail.ru>
2022-06-28 19:17:24 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void PrintName(string _Name, string _Order)
+        {
+            if (_Order == "-")
+            {
+                for (int i = _Name.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(_Name[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _Name.Length; i++)
+                {
+                    Console.WriteLine(_Name[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Введите имя");
+                string _Name = Console.ReadLine();
+                Console.WriteLine("Введите порядок вывода (\"-\" обратный)");
+                string _Order = Console.ReadLine();
+                PrintName(_Name, _Order);
+            }
+            else
+            {
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("Введите порядок вывода (\"-\" обратный)");
+                    string _Order = Console.ReadLine();
+                    PrintName(args[0], _Order);
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
